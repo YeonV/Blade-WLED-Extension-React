ecab3d1b6ca76f6b1470dd44e0022f1b998b1272
Yeon Vinzenz Varapragasam <YeonVinzenz.Varapragasam@trustedshops.de>
2020-05-27 19:02:35 +0200

    v0.7.7

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,18 +1,16 @@
 import '../../assets/img/icon-128.png';
 import '../../assets/img/yz.png';
 import { useSelector } from 'react-redux';
+import { store, persistor } from '../../lib/create-store';
 
 console.log('This is the background page.');
 window.chrome.tabs.onActivated.addListener(function (activeInfo) {
   window.chrome.tabs.get(activeInfo.tabId, function (tab) {
     console.log('This is the background page.');
     if (tab.url === 'https://wled.yeonv.com/') {
-      console.log('From BG: WLED');
       chrome.browserAction.setIcon({ path: 'yz.png' });
-      // chrome.tabs.executeScript(null, {file: 'inject.js'}, function(url) {});
     } else {
       chrome.browserAction.setIcon({ path: 'icon-128.png' });
-      console.log('From BG: NOT WLED');
     }
   });
 });
@@ -24,9 +22,9 @@ window.chrome.runtime.onMessage.addListener(function (
   console.log('GOT req', request);
   if (request.type === 'yz') {
     chrome.browserAction.setIcon({ path: 'yz.png' });
-    const ip = useSelector((state) => state.app.ip);
+    const ip = store.getState().app.ip;
     console.log('GOT YZ', ip);
-    fetch(request.url);
+    fetch(`http://${ip}${request.url}`);
   }
   if (request.type === 'yz2') {
     console.log('GOT YZ2', request);
--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,15 +1,12 @@
-console.log('test', document.querySelector('body').classList.add('yz'));
+document.querySelector('body').classList.add('yz');
 
 var anchors = document.querySelectorAll('article');
 for (let z = 0; z < anchors.length; z++) {
   let elem = anchors[z];
   elem.querySelector('.read-more').onclick = function () {
-    console.log('click', window.ip);
     chrome.runtime.sendMessage({
       type: 'yz',
-      url: `http://${window.ip}/win${elem
-        .querySelector('.entry-content')
-        .innerText.trim()}`,
+      url: `/win${elem.querySelector('.entry-content').innerText.trim()}`,
     });
   };
 }
